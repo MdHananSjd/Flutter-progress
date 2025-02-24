ef76d2e78c25e8547f4c9318d119669ab9b3dafb
Md. Hanan Sajid <mhs@mbp16.local>
2025-02-24 11:45:00 +0530

    minor updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,40 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {   //void is to specify that this function returns nothing
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build (BuildContext context){
-    return const MaterialApp(
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
-    ); //MaterialAppkk
-
+      home: Scaffold(
+        backgroundColor: Colors.blue[100],
+        body: Center(
+          child: Container(
+            height: 300,
+            width: 300,
+            decoration: BoxDecoration(
+              color: Colors.lightGreen,
+              borderRadius: BorderRadius.circular(30),
+            ),
+            padding: EdgeInsets.all(50),
+            child: Text(
+              "Hanan",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+                fontWeight: FontWeight.bold
+                ),
+              ),
+          ),
+        ),
+      ),
+    );
   }
 }
